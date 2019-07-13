78f4afa19f99a40b060e7604c7cadb0972ec5632
Alex Brown <alexbrown@comcast.net>
2019-07-13 15:56:26 -0400

    Fail 4 tests

--- src/main/java/com/zipcodewilmington/bakery/repositories/BakerRepository.java
+++ src/main/java/com/zipcodewilmington/bakery/repositories/BakerRepository.java
@@ -1,7 +1,7 @@
 package com.zipcodewilmington.bakery.repositories;
 
-import com.zipcodewilmington.bakery.models.Baker;
-import org.springframework.data.repository.CrudRepository;
+        import com.zipcodewilmington.bakery.models.Baker;
+        import org.springframework.data.repository.CrudRepository;
 
 public interface BakerRepository extends CrudRepository<Baker, Long> {
 }
--- src/main/java/com/zipcodewilmington/bakery/services/BakerService.java
+++ src/main/java/com/zipcodewilmington/bakery/services/BakerService.java
@@ -2,10 +2,17 @@ package com.zipcodewilmington.bakery.services;
 
 import com.zipcodewilmington.bakery.models.Baker;
 import com.zipcodewilmington.bakery.repositories.BakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Service
 public class BakerService {
+
     private BakerRepository repository;
 
+    @Autowired
     public BakerService(BakerRepository repository) {
         this.repository = repository;
     }
